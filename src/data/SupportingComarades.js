02e2fb78c2a24808af947b7ca60361598815f8ff
Wunna8834 <wunnaaungsayargyi99@gmail.com>
2023-03-25 21:32:45 +0630

    Finished Comarade Page

---
+++ src/data/SupportingComarades.js
@@ -0,0 +1,50 @@
+export const SUPPORT_COMARADE = [
+    {
+        id: 0,
+        photo: require('./photos/OneHand.png'),
+        title: "Monthly Bank (Su Boo)",
+        description: ""
+    },
+    {
+        id: 1,
+        photo: require('./photos/SeinShwe.png'),
+        title: "Jewelry Tickets for CDM & PDF ",
+        description: "We ran this campaign for July 22th to Aug 22th. We received 25,600,000 Kyats from this campaign."
+    },
+    {
+        id: 2,
+        photo: require('./photos/PhayMa.png'),
+        title: "Support for 5/Revolution",
+        description: "We ran this campaign for June 16th to 22th.We received 18,000,000 Kyats from this campaign."
+    },
+    {
+        id: 3,
+        photo: require('./photos/Redlips.png'),
+        title: "48 hour Campaign for Karenni",
+        description: "We ran this campaign form June 5th to 7th. We received 6,600,000 Kyats from this campaign."
+    },
+    {
+        id: 4,
+        photo: require('./photos/SAF.png'),
+        title: "Spring Tickets for SAF",
+        description: "We ran this campaign from May 14th - 23th. We received 7,313,000 Kyats from this campaign."
+    },
+    {
+        id: 5,
+        photo: require('./photos/MissionRL.png'),
+        title: "Mission Lipsticks",
+        description: "We ran this campaign from March 28th- 1st.  We received 5,000,000 Kyats from this campaign."
+    },
+    {
+        id: 6,
+        photo: require('./photos/FiveAutoGuns.png'),
+        title: "Five Automatic Guns",
+        description: "We ran this campaign from April 7th- 30th. We received 42,000,000 Kyats from this campaign."
+    },
+    {
+        id: 7,
+        photo: require('./photos/Mandat.png'),
+        title: "Revoltionary Mandat",
+        description: "We ran this campaign March 28th - 1st. We received 4,000,000 Kyats from this campaign."
+    },
+]
--- src/pages/SpringSupport.js
+++ src/pages/SpringSupport.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import { BlogComponent } from '../components/BlogComponent'
 import { PageTitle } from '../components/PageTitle'
-
+import {SUPPORT_COMARADE} from '../data/SupportingComarades'
 export const SpringSupport = () => {
-  const api = "http://localhost:3006/comrades"
   return (
     <div>
         <PageTitle pageTitle="Supporting Revolutionary Comrades"/>
         <p className='col-9 mx-auto text-center mt-4'>Supporting our people forces is our priority in this revolution. We support our defense forces and resistance groups mainly by fundraising. We established a program called "Fundraising by YTUSU" in December 2021. Since then we have been organizing many campaigns and distributed several donations.</p>
 
-        <BlogComponent api={api}/>
+        <BlogComponent links={SUPPORT_COMARADE}/>
     </div>
   )
 }
