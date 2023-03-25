83fd4dd823f9794c953f96635299690fe4979ae7
Wunna8834 <wunnaaungsayargyi99@gmail.com>
2023-03-25 22:01:43 +0630

    Fininshed Library Page

---
+++ src/data/SpringLibraryData.js
@@ -0,0 +1,20 @@
+export const SPRING_LIBRARIES = [
+    {
+        id: 0,
+        library_photo: require("../data/photos/Library1.png"),
+        library_name: "Spring Library (1)",
+        library_description: "Established in 28/11/2021 (National Day) in a village of Demoso Township , Karenni State, East of Myanmar"
+    },
+    {
+        id: 1,
+        library_photo: require("../data/photos/Library2.png"),
+        library_name: "Spring Library (2)",
+        library_description: "Established in 12/12/2021 a village in the West of Demoso Township, Karenni State, East of Myanmar"
+    },
+    {
+        id: 0,
+        library_photo: require("../data/photos/Library3.png"),
+        library_name: "Spring Library (3)",
+        library_description: "Established in 28/03/2022, in a village of Phekon Township, borderline of Southern Shan State and Karenni State"
+    },
+]
--- src/pages/SpringLibrary.js
+++ src/pages/SpringLibrary.js
@@ -4,33 +4,26 @@ import { PageTitle } from '../components/PageTitle'
 import { SpringNavBar } from '../components/SpringNavBar'
 import { getData } from '../data/Axios'
 import './css/SpringLibrary.css'
+import { SPRING_LIBRARIES } from '../data/SpringLibraryData'
+import { Card } from 'react-bootstrap'
 export const SpringLibrary = () => {
-  const [state, setState] = useState([]);
-  const api = "http://localhost:3006/spring-library"
-  useEffect(() => {
-    getData(api).then(res => {
-      console.log(res.data);
-      setState(res.data)
-    })
-  }, [])
+  
   return (
     <div>
         <PageTitle pageTitle="Spring Libraries in Karenni"/>
         <p className='col-9 mx-auto text-center mt-4'>Spring Libraries are established by YTUSU in Karenni Region (Kayah State). Up to today, there are 3 libraries located in the rural areas of this region. One place does not even have access to a phone network line and another one does not have access to internet connection. Only one has access to both networks but very unstable. These libraries are called Spring Library as these were established during the Spring Revolution of Burma. You can check the activities of these libraries also on Facebook.</p>
 
-        <div className='d-flex flex-wrap justify-content-center align-items-center gap-3 card-section py-5'>
-          {state.map(item => 
-            <LinkContainer style={{cursor: "pointer"}} to={`/read-spring-library-blog/${item.id}`}>
-              <div key={item.id} className='col-lg-3 col-md-3 col-sm-12 card-bg px-3 py-4'>
-                <div>
-                  <img className='col-12' src={item.photo} />
+        <div className='d-flex flex-wrap justify-content-center align-items-center gap-3 card-section py-3'>
+         {SPRING_LIBRARIES.map(library => 
+            <Card key={library.id} className="bg-body rounded col-lg-3 col-md-4 col-12">
+              <Card.Body>
+                <div className='col-lg-12 text-center'>
+                  <img className='col-12' src={library.library_photo}/>
                 </div>
-                <div className='mt-3'>
-                  <h6>{item.title}</h6>
-                  <p>{item.description.slice(0,65)}...</p>
-                </div>
-              </div>
-            </LinkContainer>
+                <Card.Title className='mt-2'>{library.library_name}</Card.Title>
+                <Card.Text>{library.library_description}</Card.Text>
+              </Card.Body>
+            </Card>
           )}
         </div>
     </div>
